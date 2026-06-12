660458480e3f9797d514a3d8a29cfb3c708619b7
harshit9437 <148984528+harshit9437@users.noreply.github.com>
2026-06-12 14:00:02 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        divideAndConquer(nums,0,nums.length-1);
+        return nums;
+    }
+    public void divideAndConquer(int[] nums,int low,int high){
+        if(low >=high){
+            return;
+        }
+        int mid=low + (high - low) / 2;
+        divideAndConquer(nums,low,mid);
+        divideAndConquer(nums,mid+1,high);
+        merge(nums, low, mid, high);
+    }
+    private void merge(int[] nums, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp[k++] = nums[left++];
+            } else {
+                temp[k++] = nums[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[k++] = nums[left++];
+        }
+
+        while (right <= high) {
+            temp[k++] = nums[right++];
+        }
+
+        for (int i = 0; i < temp.length; i++) {
+            nums[low + i] = temp[i];
+        }
+    }
+}
