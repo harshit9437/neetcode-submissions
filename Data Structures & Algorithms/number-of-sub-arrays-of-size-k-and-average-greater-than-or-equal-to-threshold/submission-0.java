7e16632ee1d46829d2dbe4520366c421803f2e01
harshit9437 <148984528+harshit9437@users.noreply.github.com>
2026-07-20 23:39:46 +0530

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-0

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int target = k * threshold;
+        int sum = 0;
+        int count = 0;
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        if (sum >= target) {
+            count++;
+        }
+
+        // Sliding window
+        for (int i = k; i < arr.length; i++) {
+            sum += arr[i];
+            sum -= arr[i - k];
+
+            if (sum >= target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
