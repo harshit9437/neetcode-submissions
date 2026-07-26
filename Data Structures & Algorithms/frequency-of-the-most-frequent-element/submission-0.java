c73bbc2f14d1e840dc97741a58a271ce460ec9e3
harshit9437 <148984528+harshit9437@users.noreply.github.com>
2026-07-26 23:34:42 +0530

    Add: frequency-of-the-most-frequent-element - submission-0

---
+++ Data Structures & Algorithms/frequency-of-the-most-frequent-element/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        long total = 0;
+        int ans = 1;
+
+        for (int right = 0; right < nums.length; right++) {
+            total += nums[right];
+
+            while ((long) nums[right] * (right - left + 1) - total > k) {
+                total -= nums[left];
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+
+    }
+}
